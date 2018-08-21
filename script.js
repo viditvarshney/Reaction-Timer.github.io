740038fd320fff3a84002dc76af10277cf8a47b8
Waferio <34159717+viditvarshney@users.noreply.github.com>
2018-08-21 18:32:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,72 +1,27 @@
-            var start = new Date().getTime();
-            
-            function getRandomColor() {
-                
-                var x = Math.floor(Math.random()* 256);
-                var y = Math.floor(Math.random()* 256);
-                var z = Math.floor(Math.random()* 256);
-                
-                var randomColorMaker = "rgb(" + x + "," + y + "," + z + ")";
-            
-                document.getElementById("shape").style.backgroundColor = randomColorMaker;
-            }
-            
-            function makeShapeAppear() {
-                
-                var top = Math.random()* 400;
-                
-                var left =Math.random()* 1150;
-                
-                var width = (Math.random()* 400) + 100;
-                
-                if(Math.random() < 0.5) {
-                    
-                    document.getElementById("shape").style.borderRadius = "50%";
-                } else {
-                    
-                    document.getElementById("shape").style.borderRadius = "0";
-                }
-                
-                document.getElementById("shape").style.top = top + "px";
-                document.getElementById("shape").style.left = left + "px";
-                document.getElementById("shape").style.width = width + "px";
-                document.getElementById("shape").style.height = width + "px";
-                
-                document.getElementById("shape").style.backgroundColor = getRandomColor();
-                
-                document.getElementById("shape").style.display ="block";
-                start = new Date().getTime();
-            }
-            
-            function Timeout() {
-                
-                setTimeout(makeShapeAppear, Math.random()* 2000);
+            var canvas = document.getElementById("canvas1");
+            var ctx = canvas.getContext("2d");
+            ctx.
+
+
+
+            function openNav() {
+                document.getElementById("mySidenav").style.width = "250px";
+                document.getElementById("main").style.marginLeft = "250px";
                 
             }
-            Timeout();
+
+            function closeNav() {
+                document.getElementById("mySidenav").style.width = "0";
+                document.getElementById("main").style.marginLeft= "0";
+                
+            }
+
+            $('#me').click(function() {
+                
+                alert("Hey!, I am Vidit.. I love to write CODE, I always say : First, solve the problem. Then, write the code., To know more about me , go to my facebook profile... ");
+            });
+
+             
+
             
         
-            document.getElementById("shape").onclick = function() {
-                
-                document.getElementById("shape").style.display = "none";
-                
-                var end = new Date().getTime();
-                
-                var timeTaken = (end - start) / 1000 + " sec";
-                
-                document.getElementById("timeTaken").innerHTML = timeTaken;
-                
-                    var bestTime = 0; 
-                
-                    if (timeTaken < bestTime) {
-                        
-                        bestTime = timeTaken;
-                        document.getElementById("best").innerHTML = bestTime + " sec";
-                    } else {
-                        
-                        document.getElementById("best").innerHTML = timeTaken + " sec";
-                    }
-                
-                
-                Timeout();
-            }
